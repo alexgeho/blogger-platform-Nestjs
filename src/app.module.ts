f49d83789c96ba1696638a41d018626524b6adb7
Alexander Gerhard <870717ag@gmail.com>
2025-10-20 18:11:39 +0200

    feat: continue setting up Blogger Platform project with NestJS structure and MongoDB connection, creating entity user part 1

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,9 +2,13 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { UserAccountsModule } from './modules/user-accounts/user-accounts.module';
+import { MongooseModule } from '@nestjs/mongoose';
 
 @Module({
-  imports: [UserAccountsModule],
+  imports: [
+    MongooseModule.forRoot('mongodb://localhost/nest-blogger-platform'), // Укажите свой URL MongoDB
+    UserAccountsModule,
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
