2d1c255c8e9640784607fbb22026d6dd992c8bc8
Alexander Gerhard <870717ag@gmail.com>
2025-10-23 16:56:59 +0200

    feat(users): first successful registration in MongoDB using NestJS 🚀

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,15 +1,31 @@
-import { Module } from '@nestjs/common';
+import { Module, OnModuleInit } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { UserAccountsModule } from './modules/user-accounts/user-accounts.module';
-import { MongooseModule } from '@nestjs/mongoose';
+import { MongooseModule, InjectConnection } from '@nestjs/mongoose';
+import mongoose, { Connection } from 'mongoose';
 
 @Module({
   imports: [
-    MongooseModule.forRoot('mongodb://localhost/nest-blogger-platform'), // Укажите свой URL MongoDB
+    MongooseModule.forRoot(
+      'mongodb+srv://user2:rlTZYUNTj0234PGu@cluster0.zgjfrlf.mongodb.net/NestBloggerPlatform?retryWrites=true&w=majority&appName=Cluster0',
+    ),
     UserAccountsModule,
   ],
   controllers: [AppController],
   providers: [AppService],
 })
-export class AppModule {}
+export class AppModule implements OnModuleInit {
+  constructor(@InjectConnection() private readonly connection: Connection) {}
+
+  onModuleInit() {
+    if (this.connection.readyState === mongoose.ConnectionStates.connected) {
+      console.log('✅ Connected to MongoDB');
+    } else {
+      console.log(
+        '❌ MongoDB not connected. State:',
+        this.connection.readyState,
+      );
+    }
+  }
+}
