6a92504c4ce094364009851d365dc3c0d6275bfb
Alexander Gerhard <870717ag@gmail.com>
2025-10-29 22:44:55 +0100

    feat(auth): login, part 2

--- src/modules/user-accounts/application/auth.service.ts
+++ src/modules/user-accounts/application/auth.service.ts
@@ -1,15 +1,18 @@
 import { InjectModel } from '@nestjs/mongoose';
 import { LoginDto } from '../dto/loginDto';
+import { JwtService } from '@nestjs/jwt';
 import { User } from '../domain/user.entity';
 import { Injectable } from '@nestjs/common';
 import { AuthRepository } from '../infrastructure/auth.repository';
 import bcrypt from 'bcrypt';
+import { UsersRepository } from '../infrastructure/user.repository';
 
 @Injectable()
 export class AuthService {
   constructor(
-    @InjectModel(User.name)
-    private authRepository: AuthRepository,
+    private usersRepository: UsersRepository,
+    private jwtService: JwtService,
+    private cryptoService: CryptoService,
   ) {}
 
   async login(dto: LoginDto): Promise<{ accessToken: string }> {
